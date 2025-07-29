6908f5a51b2625b0168f83024003d645406d0f3d
seccion6944 <sebastiamnr@gmail.com>
2025-07-28 20:27:31 -0500

    correciones en firebase y suparbase

--- src/componentes/firebase.js
+++ src/componentes/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_FIREBASE_APP_ID,
-    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+    apiKey: "AIzaSyBv640u9qQDfm2me_ovO15_j8xnDfYbeOY",
+    authDomain: "threads-661d8.firebaseapp.com",
+    projectId: "threads-661d8",
+    storageBucket: "threads-661d8.firebasestorage.app",
+    messagingSenderId: "109993270098",
+    appId: "1:109993270098:web:bd555387f4d1b529e4b88b",
+    measurementId: "G-4CD7SFCTTJ"
 };
 
 const app = initializeApp(firebaseConfig);
--- src/componentes/suparbase.js
+++ src/componentes/suparbase.js
@@ -1,7 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
-
-const supabase = createClient(supabaseUrl, supabaseAnonKey);
+const supabase = createClient('https://njomxsohpzexxkvpfkzt.supabase.co',
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5qb214c29ocHpleHhrdnBma3p0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI3Nzk0OTAsImV4cCI6MjA2ODM1NTQ5MH0.rvWTrPOFPf_hlPEIWcAcmHVqVWEw_ACB_S-1nbtDr90'
+);
 export default supabase;
